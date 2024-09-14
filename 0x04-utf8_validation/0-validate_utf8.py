feadd0a8046d2d2be78a6daf6d32f3390c2d9e2b
GodsGiftLove <emefak77@gmail.com>
2024-09-14 11:29:08 +0000

    utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,59 +1,39 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
-
+"""UTF-8 Validation"""
 
 def validUTF8(data):
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
-    Determines given data set represents valid UTF-8 encoding
 
-    Args:
-        data (list): list of integers representing 1 byte of data each
+    number_bytes = 0
 
-    Returns:
-        bool: True if data is valid UTF-8 encoding, else False
-    """
-    # Variable to track number of remaining bytes in multi-byte character
-    remaining_bytes = 0
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    for byte in data:
-        # Extract the 8 least significant bits
-        byte &= 0xFF
+    for i in data:
 
-        # If it's the start of a new character
-        if remaining_bytes == 0:
-            if byte >> 7 == 0:
-                continue  # Single-byte character, move to the next byte
+        mask_byte = 1 << 7
 
-            # Determine the number of bytes in the character
-            if byte >> 5 == 0b110:
-                remaining_bytes = 1
-            elif byte >> 4 == 0b1110:
-                remaining_bytes = 2
-            elif byte >> 3 == 0b11110:
-                remaining_bytes = 3
-            else:
-                return False  # Invalid start of a multi-byte character
+        if number_bytes == 0:
 
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+            
         else:
-            # If it's a continuation byte (10xxxxxx)
-            if byte >> 6 == 0b10:
-                remaining_bytes -= 1
-            else:
-                return False  # Invalid continuation byte
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+            
+        number_bytes -= 1
 
-    # If there are remaining bytes at the end, it's an incomplete sequence
-    return remaining_bytes == 0
-
-
-if __name__ == "__main__":
-    # Test cases
-    data1 = [65]
-    print(validUTF8(data1))  # True
-
-    data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data2))  # True
-
-    data3 = [229, 65, 127, 256]
-    print(validUTF8(data3))  # False
+    if number_bytes == 0:
+        return True
+    
+    return False
